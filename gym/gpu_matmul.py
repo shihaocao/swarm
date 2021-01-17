6a1778a3a6217860bd4eca44e4148dd40ca149be
Shihao Cao <shehowcow@gmail.com>
2021-01-17 03:28:22 -0500

    add gpu testing script

---
+++ gym/gpu_matmul.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+tf.debugging.set_log_device_placement(True)
+
+# Create some tensors
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+
+print(c)
+
+# now run on cpu instead:
+
+# Place tensors on the CPU
+with tf.device('/CPU:0'):
+    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+
+    # Run on the CPU
+    c = tf.matmul(a, b)
+    print(c)
